d1234a16e67d5334967c0dea0a8a2ec4117bc3b4
syrmon <mmehemmsd@gmail.com>
2024-04-09 13:05:01 +0400

    removed google tag

--- app/[locale]/layout.js
+++ app/[locale]/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
+
 export const metadata = {
   title: "Rent a cars In Baku",
   description: "The best price perfomant rent a car in Baku",
@@ -13,15 +14,6 @@ export default function LocaleLayout({ children, params: { locale } }) {
     <html lang={locale}>
       <head>
         <link rel="icon" href="../favicon.ico" sizes="any" />
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=AW-16524695155"
-        ></script>
-        <script>
-          window.dataLayer = window.dataLayer || []; function gtag()
-          {dataLayer.push(arguments)}
-          gtag('js', new Date()); gtag('config', 'AW-16524695155');
-        </script>
       </head>
       <body>{children}</body>
     </html>
