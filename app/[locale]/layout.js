91b94c81863b4aa9408c0fc52d580ff9c3341c30
syrmon <mmehemmsd@gmail.com>
2024-04-28 23:58:34 +0400

    updated script tag

--- app/[locale]/layout.js
+++ app/[locale]/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Script from "next/script";
+import NoScript from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,20 +13,19 @@ export const metadata = {
 export default function LocaleLayout({ children, params: { locale } }) {
   return (
     <html lang={locale}>
-      <Script id="metrika-counter" strategy="afterInteractive">
+      <Script type="text/javascript">
         {`(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-          m[i].l=1*new Date();
-          for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-          k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-          (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
- 
-          ym(97137271, "init", {
-                defer: true,
-                clickmap:true,
-                trackLinks:true,
-                accurateTrackBounce:true,
-                webvisor:true
-          });`}
+        m[i].l=1*new Date();
+        for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+        ym(97137271, "init", {
+             clickmap:true,
+             trackLinks:true,
+             accurateTrackBounce:true,
+             webvisor:true
+        });`}
       </Script>
       <body>{children}</body>
     </html>
