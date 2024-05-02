d970df65cb17b9cb1199231aab430fe6c0ee61e2
syrmon <mmehemmsd@gmail.com>
2024-05-02 17:09:18 +0400

    added yandex verification

--- app/[locale]/layout.js
+++ app/[locale]/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Script from "next/script";
+import Head from "next/head"
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -11,29 +12,32 @@ export const metadata = {
 export default function LocaleLayout({ children, params: { locale } }) {
   return (
     <html lang={locale}>
-      <Script type="text/javascript">
-        {`(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+      <head>
+        <script type="text/javascript">
+          {`(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
         m[i].l=1*new Date();
         for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
         k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
         (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-
+        
         ym(97137271, "init", {
-             clickmap:true,
-             trackLinks:true,
-             accurateTrackBounce:true,
-             webvisor:true
+          clickmap:true,
+          trackLinks:true,
+          accurateTrackBounce:true,
+          webvisor:true
         });`}
-      </Script>
-      <noscript>
-        <div>
-          <img
-            src="https://mc.yandex.ru/watch/97137271"
-            style={{ position: "absolute", left: "-9999px" }}
-            alt=""
-          />
-        </div>
-      </noscript>
+        </script>
+        <meta name="yandex-verification" content="b17f4878682a8629" />
+        <noscript>
+          <div>
+            <img
+              src="https://mc.yandex.ru/watch/97137271"
+              style={{ position: "absolute", left: "-9999px" }}
+              alt=""
+            />
+          </div>
+        </noscript>
+      </head>
       <body>{children}</body>
     </html>
   );
